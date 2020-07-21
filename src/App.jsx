63d3ec50e1a32c87c1a8d45f079330478011df4f
carlosiib <ntic.carlos@gmail.com>
2020-07-21 13:24:21 -0700

    Agegar component for new messages in the chat

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import React, { useContext } from 'react';
 import { ChatContext } from './context/ChatProvider';
 
 import Navbar from "./components/Navbar"
+import Chat from "./components/Chat"
 
 function App() {
   const { usuario } = useContext(ChatContext)
   return usuario !== null ? (
     <div>
       <Navbar />
+      {
+        usuario.estado ? (
+          <Chat />
+        ) : (
+            <div className="lead text-center mt-3">Debes iniciar sesión</div>
+          )
+      }
 
     </ div>
   ) : (
